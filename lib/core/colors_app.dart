fcc759b5cb10bf45972c9622f7691804b12ce681
qmar <qmara21@gmail.com>
2023-05-16 12:32:02 +0300

    add font

---
+++ lib/core/colors_app.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class   ColorsApp{
+  static const Color white = Color(0xFFFFFFFF);
+
+}
---
+++ lib/core/values_app.dart
@@ -0,0 +1,19 @@
+class SizeApp{
+  static const double s8=8.0;
+  static const double s10=10.0;
+  static const double s12=12.0;
+  static const double s14=14.0;
+  static const double s16=16.0;
+  static const double s18=18.0;
+  static const double s20=20.0;
+}
+
+class PaddingApp{
+  static const double p8=8.0;
+  static const double p10=10.0;
+  static const double p12=12.0;
+  static const double p14=14.0;
+  static const double p16=16.0;
+  static const double p18=18.0;
+  static const double p20=20.0;
+}
